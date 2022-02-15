07f874dd2ec71fe6a9de79d2959a1b2852887784
dubrovsky404 <n.dubrovsky404@gmail.com>
2022-02-15 14:33:12 +0300

    Added possibility of deleting notes, remade project structure

--- Notes/Program.cs
+++ Notes/Program.cs
@@ -18,7 +18,7 @@ namespace Notes
                 switch (selectedAction.ToLower())
                 {
                     case "1":
-                        ShowNotes();
+                        PerformAction(Actions.ShowNotes);
                         break;
                     case "2":
                         AddNewNote();
@@ -26,6 +26,7 @@ namespace Notes
                     case "3":
                         break;
                     case "4":
+                        PerformAction(Actions.DeleteNote);
                         break;
                     case "x":
                         isProgramRunning = false;
@@ -37,21 +38,14 @@ namespace Notes
             }
         }
 
-        private static void ShowNotes()
+        private static void PerformAction(Actions action)
         {
-            Console.Clear();
-            if (_notesList.Count == 0)
-            {
-                Console.WriteLine("There are no notes yet!");
-                Console.ReadKey();
-                return;
-            }
-
             while (true)
             {
-                Console.Clear();
-                UI.ShowNotesList(_notesList);
-                Console.WriteLine("Enter the number of note to read (x - return to main menu)");
+                bool isReturnedToMainMenu = UI.ShowNotesList(_notesList, action);
+                if (isReturnedToMainMenu)
+                    return;
+
                 string selectedAnswer = Console.ReadLine();
                 if (selectedAnswer.ToLower() == "x")
                     return;
@@ -61,10 +55,45 @@ namespace Notes
                 if (noteNumber != 0 && noteNumber - 1 < _notesList.Count)
                 {
                     UI.ShowNote(_notesList[noteNumber - 1]);
-                    Console.ReadKey();
+                    switch (action)
+                    {
+                        case Actions.ShowNotes:
+                            Console.ReadKey();
+                            break;
+                        case Actions.DeleteNote:
+                            DeleteNote(noteNumber - 1);
+                            break;
+                    }
                 }
                 else
                     UI.ShowWrongCommandMessage();
+            }
+        }
+
+        private static void DeleteNote(int noteIndex)
+        {
+            bool isQuestionShown = true;
+            while (isQuestionShown)
+            {
+                UI.ShowNote(_notesList[noteIndex]);
+                UI.ShowDeleteNoteDialog(); // "Do you want to delete this note?"
+                string selectedAnswer = Console.ReadLine();
+                switch (selectedAnswer)
+                {
+                    // 1 - yes
+                    case "1":
+                        isQuestionShown = false;
+                        _notesList.Remove(_notesList[noteIndex]);
+                        UI.ShowNoteDeletedMesage();
+                        break;
+                    // 2 - no
+                    case "2":
+                        isQuestionShown = false;
+                        break;
+                    default:
+                        UI.ShowWrongCommandMessage();
+                        break;
+                }
             }
         }
 
--- Notes/UI.cs
+++ Notes/UI.cs
@@ -6,6 +6,13 @@ namespace Notes
 {
     static class UI
     {
+        private static Dictionary<Actions, string> ActionsTexts = new Dictionary<Actions, string>()
+        {
+            { Actions.ShowNotes, "Enter the number of note to read (x - return to main menu)" },
+            { Actions.DeleteNote, "Enter the number of note to delete (x - return to main menu)" },
+            { Actions.EditNote, "Enter the number of note to edit (x - return to main menu)" }
+        };
+
         public static void ShowMainMenu()
         {
             Console.Clear();
@@ -16,7 +23,7 @@ namespace Notes
             Console.WriteLine("1 - Show notes");
             Console.WriteLine("2 - Create note");
             Console.WriteLine("3 - Edit note (not available!)");
-            Console.WriteLine("4 - Delete note (not available!)");
+            Console.WriteLine("4 - Delete note");
             Console.WriteLine("x - Exit\r\n");
         }
 
@@ -29,11 +36,22 @@ namespace Notes
             Console.ReadKey();
         }
 
-        public static void ShowNotesList(List<Note> notesList)
+        public static bool ShowNotesList(List<Note> notesList, Actions action)
         {
+            Console.Clear();
+            if (notesList.Count == 0)
+            {
+                Console.WriteLine("There are no notes yet!");
+                Console.ReadKey();
+                return true;
+            }
+
             for (int i = 0; i < notesList.Count; i++)
                 Console.WriteLine($"{i + 1}. {notesList[i].Title}");
             Console.WriteLine();
+
+            Console.WriteLine(ActionsTexts[action]);
+            return false;
         }
 
         public static void ShowNote(Note note)
@@ -59,6 +77,23 @@ namespace Notes
         {
             Console.Clear();
             Console.WriteLine("The note was saved!");
+            Console.ReadKey();
+        }
+
+        public static void ShowDeleteNoteDialog()
+        {
+            Console.WriteLine();
+            Console.WriteLine("+------------------------------------+");
+            Console.WriteLine("|  Do you want to delete this note?  |");
+            Console.WriteLine("|  1 - yes                           |");
+            Console.WriteLine("|  2 - no                            |");
+            Console.WriteLine("+------------------------------------+\r\n");
+        }
+
+        public static void ShowNoteDeletedMesage()
+        {
+            Console.Clear();
+            Console.WriteLine("The note was deleted!");
             Console.ReadKey();
         }
     }
